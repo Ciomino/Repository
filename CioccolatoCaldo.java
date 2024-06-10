ff2622bf698c3359c5883964f6511827097336a6
C4rra <maso.sith@gmail.com>
2024-06-10 18:04:51 +0200

    Create CioccolatoCaldo.java

---
+++ CioccolatoCaldo.java
@@ -0,0 +1,15 @@
+public class CioccolatoCaldo extends Cioccolato {
+    private double temperatura;
+    private double densita;
+    public CioccolatoCaldo(String tipo, double percentuale,String aggiunte,double costo,double temperatura,double densita){
+        super(tipo,percentuale,aggiunte,costo=20);
+        this.temperatura = temperatura;
+        this.densita = densita;
+    }
+    
+        
+    
+   
+
+    
+}
